279af2e8709e4527b3be9436afc57640bb80208f
jaspreet kaur <99533669+jass-1602@users.noreply.github.com>
2022-03-02 16:55:14 +0530

    Add files via upload

---
+++ try_hoee.py
@@ -0,0 +1,35 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+import time
+
+# Replace below path with the absolute path
+browser = webdriver.Firefox()
+
+phone = ["+919501601400","+917528897778",'+919915244444']
+message = "questo è il messaggio"
+
+for i in phone:
+    url = "https://web.whatsapp.com/send?phone="+ i + "&text=" + message + "&app_absent=1"
+    time.sleep(5)
+
+    # Load Whatsapp Web page
+    browser.get(url)
+    # time.sleep(5)
+
+    # Wait for the page be loaded
+    time.sleep(10)
+
+    enter_action = ActionChains(browser)
+    time.sleep(5)
+    enter_action.send_keys(Keys.ENTER)
+    time.sleep(5)
+
+# Send message
+    enter_action.perform()
+    time.sleep(5)
+
+
+# Close browser
+# browser.close()
